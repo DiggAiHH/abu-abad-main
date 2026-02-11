f0aacded03ebc46d7a62a8485ffebba8d69dd864
MM <mm@air-von-mm.speedport.ip>
2026-02-11 10:29:04 +0100

    fix(frontend): Added missing VideoCallProps interface to VideoCall

--- src/pages/VideoCall.tsx
+++ src/pages/VideoCall.tsx
@@ -1,7 +1,11 @@
 interface VideoCallProps {}
 
-function VideoCall({}: VideoCallProps) {
-  // Komponentencode hier
+function VideoCall({}: VideoCallProps): JSX.Element {
+  return (
+    <div>
+      {/* Komponentencode hier */}
+    </div>
+  );
 }
 
 export default VideoCall;
