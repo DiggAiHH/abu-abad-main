bdd07430e45a99f281c866ef9ca46cfbd4cda85c
MM <mm@air-von-mm.speedport.ip>
2026-02-12 03:29:09 +0100

    fix(frontend): define AppProps interface for App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-interface AppProps {}
+import React from 'react';
 
-function App(): JSX.Element {
+interface AppProps {
+  // Definieren Sie hier die zukünftigen Props, falls benötigt
+}
+
+const App: React.FC<AppProps> = () => {
   return (
     <div>
       <h1>Willkommen bei Abu-Abad</h1>
     </div>
   );
-}
+};
 
 export default App;
